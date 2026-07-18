ecc4f8fdc29fef68c154b0a75de2d6149cf2c189
l5partanl <caorlingiancarlo@gmail.com>
2026-07-18 11:13:14 +0200

    Add 3d object on about section

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
+import { Model3d } from '../model3d/model3d';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Model3d],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
