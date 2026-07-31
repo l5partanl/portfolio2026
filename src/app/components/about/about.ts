93866cc6dea49c5a0c2785c998d8c57d2775c499
l5partanl <caorlingiancarlo@gmail.com>
2026-07-31 18:08:10 +0200

    Added CV download in About section

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -9,4 +9,27 @@ import { Model3d } from '../model3d/model3d';
 })
 export class About {
   @Input() active = false;
+
+  showCvTooltip = false;
+
+  tooltipX = 0;
+  tooltipY = 0;
+
+  moveCvTooltip(event: MouseEvent) {
+    const rect = (event.currentTarget as HTMLElement).getBoundingClientRect();
+
+    this.tooltipX = event.clientX - rect.left + 20;
+
+    this.tooltipY = event.clientY - rect.top + 20;
+  }
+
+  downloadCV() {
+    const link = document.createElement('a');
+
+    link.href = '/assets/CV-GianCaorlin.pdf';
+
+    link.download = 'CV-GianCaorlin.pdf';
+
+    link.click();
+  }
 }
